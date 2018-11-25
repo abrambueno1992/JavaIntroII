f773eaf9602521c1b6e297785e14d557782087f0
Abraham Bueno <abueno@localhost.localdomain>
2018-11-25 14:25:12 -0600

    Playing around with booleans and characters, including unicode

--- src/com/abrahambueno/Main.java
+++ src/com/abrahambueno/Main.java
@@ -3,22 +3,17 @@ package com.abrahambueno;
 public class Main {
 
     public static void main(String[] args) {
-    	// width of int = 32 bits (4 bytes).
-    	int myIntValue = 5 / 3;
 
-    	// width of float = 32 bits (4 bytes)
-    	float myFloatValue = 5f / 3f;
+    	// width of 16 bits (2 bytes)
+    	char myChar = '\u00A9';
+		System.out.println(myChar);
 
-    	// width of double = 64 bits (8 bytes)
-    	double myDoubleValue = 5d / 3;
-    	System.out.println("myIntValue = " + myIntValue);
-    	System.out.println("myFloatValue = " + myFloatValue);
-    	System.out.println("myDoubleValue = " + myDoubleValue);
+		boolean myBoolean = true;
+		boolean isMale = true;
 
-    	double wLb = 3;
-    	double wKg = 3 * 0.45359237;
-    	System.out.println(wKg);
+		char myReg = '\u00AE';
 
+		System.out.println("Registered symbol = " + myReg);
 
     }
 }
