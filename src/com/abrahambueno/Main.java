b91dfd08ce5b34dc60c70f39270ac2ea88f5a2ea
Abraham Bueno <abueno@localhost.localdomain>
2018-11-25 07:25:54 -0600

    Better example of different float types, versus int

--- src/com/abrahambueno/Main.java
+++ src/com/abrahambueno/Main.java
@@ -3,9 +3,9 @@ package com.abrahambueno;
 public class Main {
 
     public static void main(String[] args) {
-    	int myIntValue = 5;
-    	float myFloatValue = 5f;
-    	double myDoubleValue = 5d;
+    	int myIntValue = 5 / 3;
+    	float myFloatValue = 5f / 3;
+    	double myDoubleValue = 5d / 3;
     	System.out.println("myIntValue = " + myIntValue);
     	System.out.println("myFloatValue = " + myFloatValue);
     	System.out.println("myDoubleValue = " + myDoubleValue);
