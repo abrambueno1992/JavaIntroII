2364af62b0f50b364f38157ea81375353afef988
Abraham Bueno <abueno@localhost.localdomain>
2018-12-03 22:02:28 -0600

    Leap year method

--- src/com/abrahambueno/Main.java
+++ src/com/abrahambueno/Main.java
@@ -21,6 +21,20 @@ public class Main {
             return false;
         }
     }
+
+    public static boolean isLeapYear(int year) {
+        if (year < 1 || year > 9999) {
+            return false;
+        }
+        if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        } else if (year % 400 == 0) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
     public static void main(String[] args) {
 
     }
