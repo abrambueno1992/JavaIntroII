6c2b9b96a67ae6933b78e2c7bc21c8ad188045e1
Abraham Bueno <abueno@localhost.localdomain>
2018-12-03 22:01:23 -0600

    Megabytes converter

--- src/com/abrahambueno/Main.java
+++ src/com/abrahambueno/Main.java
@@ -1,34 +1,18 @@
 package com.abrahambueno;
 
 public class Main {
-    public static double calcFeetAndInchesToCentimeters(int feet, int inches) {
-        if (feet <= 0 && inches <= 0) {
-            return -1;
+    public static void printMegaBytesAndKiloBytes(int kiloBytes) {
+        if (kiloBytes < 0) {
+            System.out.println("Invalid Value");
+            return;
         }
-        if (inches <= 0 && feet <= 0 || inches >= 12) {
-            return -1;
-        }
-        double totalInches = (feet * 12) + inches;
-        double totalCentimeters = totalInches * 2.54;
-        return  totalCentimeters;
-    }
-
-    public static double calcFeetAndInchesToCentimeters(int inches) {
-        if (inches <= 0 || inches >= 12) {
-            return -1;
-        }
-        int remainingInches = inches % 12;
-        int feet = inches / 12;
-        System.out.println("remaining inch, feet" + remainingInches + " " +feet);
-        double centimeters = calcFeetAndInchesToCentimeters(feet, remainingInches);
-        return  centimeters;
-//        System.out.println("centimeters" + centimeters);
+        int XX = kiloBytes;
+        int YY = (int) XX / 1024;
+        int ZZ = (int) XX % 1024;
+        System.out.println(XX + " KB = " + YY + " MB " + "and " + ZZ + " KB");
     }
     public static void main(String[] args) {
-        double result = calcFeetAndInchesToCentimeters(10);
-        System.out.println(result);
-        result = calcFeetAndInchesToCentimeters(3, 10);
-        System.out.println(result);
+        
     }
 
 }
